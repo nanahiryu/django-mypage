cf74096b037b13b1fccfeb9f72ba72d348777ff9
nanahiryu <ryu2751keio@gmail.com>
2021-01-22 12:51:22 +0900

    create subsection

--- mypage/urls.py
+++ mypage/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView, AboutView
+from .views import IndexView, AboutView, EveresView
 
 
 urlpatterns = [
     path('', IndexView.as_view()),
     path('about', AboutView.as_view()),
+    path('everes', EveresView.as_view()),
 ]
